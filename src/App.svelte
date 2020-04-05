1dfa26757fd022516d7fe30ae088db51b3e3444e
breadthe <omigoshdev@protonmail.com>
2020-04-05 17:40:29 -0500

    Add & configure TailwindCSS + PurgeCSS

--- src/App.svelte
+++ src/App.svelte
@@ -5,24 +5,15 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p class="mt-8">Starter template containing:
+		<ul class="font-bold text-2xl">
+			<li style="color: #ff3e00">Svelte</li>
+			<li class="text-blue-800">Electron</li>
+			<li class="text-teal-600">TailwindCSS</li>
+			<li class="text-green-500">PurgeCSS</li>
+			<li class="text-orange-500">SCSS</li>
+		</ul>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
