574c0b25482936527981d2b2fafc363e9e289129
breadthe <omigoshdev@protonmail.com>
2020-11-29 14:51:59 -0600

    Upgrade Tailwind to 2.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,7 @@
-const production = !process.env.ROLLUP_WATCH;
-const purgecss = require("@fullhuman/postcss-purgecss");
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
-    plugins: [
-        require("postcss-import")(),
-        require("tailwindcss"),
-        require("autoprefixer"),
-        tailwindcss('./tailwind.config.js'),
-        // Only purge css on production
-        production &&
-        purgecss({
-            content: ["./**/*.html", "./**/*.svelte"],
-            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        })
-    ]
-};
+    plugins: {
+        "postcss-import": {},
+        tailwindcss: {},
+        autoprefixer: {},
+    }
+}
